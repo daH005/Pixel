8d59f57b018fa1e35cabb5b4abd1e3ab30415c1f
danil <danil.shevelev.2004@mail.ru>
2024-01-10 03:58:56 +0500

    [fix] убрал `--noconsole` для редактора

--- building/exe/editor/main.py
+++ building/exe/editor/main.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
     PyInstaller.__main__.run([
         '../../../editor/main.py',
         '--onefile',
-        '--noconsole',
         '--paths=../../../game;../../../;../../../editor',
         '--distpath=../../../editor',
         '--name=Editor',
